651b4705c1d7d8b82553ce39032c047972e5b934
rttmoa <rttmoa@gmail.com>
2023-03-09 21:58:46 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -55,8 +55,9 @@ module.exports = {
           __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
         })
     ],
- 
+    
     devServer: {
+        // https://www.webpackjs.com/configuration/dev-server/#devserverproxy
         proxy: {
           // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
           // koa 代码在 ./mock 目录中，启动命令为 npm run mock
